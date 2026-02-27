9fb58c53ba809c995540739e5e0626ba8f85be8f
keralatreasury <sdwan.keralatreasury@gmail.com>
2026-02-26 23:11:45 -0800

    Update main-script.js

--- js/main-script.js
+++ js/main-script.js
@@ -413,7 +413,7 @@ if (contentFrame) {
 // Register service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('/network/sw.js')
             .then(registration => {
                 console.log('ServiceWorker registered successfully with scope:', registration.scope);
             })
