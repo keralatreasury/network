0f1d96db91139f6533d5234d0f096c33a67d18da
keralatreasury <sdwan.keralatreasury@gmail.com>
2026-05-05 17:24:42 +0530

    Add files via upload

--- blog/config.js
+++ blog/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     
     // Apps Script Web App URL (for writes - likes/comments)
     // DEPLOY THIS FIRST: Go to Extensions > Apps Script, paste code.gs, Deploy > New deployment > Web App
-    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzmacJT4qncbsyT-enhzqug7mHdkN25qKONhb8Tws81fBfiFSzwkaOI1BLCF4pwNjf-/exec',
+    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwTro39ZWxNsl1s6TMBmImlZJXnxN56LBQWaaPeVirXmmh-8hD8FsvnqcbXhSPyQuko_w/exec',
     
     // Sheet names
    
